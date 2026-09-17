97f9dfedd734be5cfd330a0f15bcae4a73dc3978
medeiros <guigsventura@gmail.com>
2026-09-17 13:34:47 -0300

    Primeiro commit do aplicativo

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+void main () {
+    byte idade = 10;
+if (idade <= 11 ) {
+System.out.println("Crianca");
+} else if (idade <= 10) {
+   System.out.println("Crianca");
+} else if (idade <=17) {
+    System.out.println("Adolescente)");
+}
+
+}
